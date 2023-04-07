788a225092c1f07ec80575555a82a637bd9c3ef5
Anton2050 <avvsoft2050@gmail.com>
2023-04-07 20:15:40 +0300

    remove unnecessary dependencies

--- app/src/main/java/com/avvsoft2050/githubtool/data/api/ApiFactory.kt
+++ app/src/main/java/com/avvsoft2050/githubtool/data/api/ApiFactory.kt
@@ -1,7 +1,6 @@
 package com.avvsoft2050.githubtool.data.api
 
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiFactory {
