3ed67d18eec96ac223c6a2a925efacdb6660da42
Anton2050 <avvsoft2050@gmail.com>
2023-04-07 19:56:38 +0300

    remove unnecessary dependencies

--- app/src/main/java/com/avvsoft2050/githubtool/data/db/LoadedRepoDatabase.kt
+++ app/src/main/java/com/avvsoft2050/githubtool/data/db/LoadedRepoDatabase.kt
@@ -8,16 +8,16 @@ import com.avvsoft2050.githubtool.data.db.dao.LoadedRepoDao
 import com.avvsoft2050.githubtool.data.db.entity.LoadedRepo
 
 @Database(entities = arrayOf(LoadedRepo::class), version = 1, exportSchema = false)
-public abstract class LoadedRepoDatabase : RoomDatabase(){
+abstract class LoadedRepoDatabase : RoomDatabase() {
 
-    abstract fun loadedRepoDao() : LoadedRepoDao
+    abstract fun loadedRepoDao(): LoadedRepoDao
 
-    companion object{
+    companion object {
         @Volatile
         private var INSTANCE: LoadedRepoDatabase? = null
 
-        fun getDatabase(context: Context) : LoadedRepoDatabase {
-            return INSTANCE ?: synchronized(this){
+        fun getDatabase(context: Context): LoadedRepoDatabase {
+            return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     LoadedRepoDatabase::class.java,
